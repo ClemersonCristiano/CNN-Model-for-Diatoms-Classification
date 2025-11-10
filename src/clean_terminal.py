18198e1dbb94fd005c80b52589a862704ec614e6
Clemerson Cristiano <clemersoncristiano20@gmail.com>
2025-11-09 22:18:28 -0300

    Arquivo Movido da Pasta src para CNN

--- src/clean_terminal.py
+++
@@ -1,5 +0,0 @@
-import os
-def clean_terminal():
-    """Limpa a tela do terminal, compatível com Windows, macOS e Linux."""
-    # Para Windows, o comando é 'cls'. Para macOS e Linux, é 'clear'.
-    os.system('cls' if os.name == 'nt' else 'clear')
